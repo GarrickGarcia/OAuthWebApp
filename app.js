6beb29d77936270e1abccff9fa798656f484b389
Garrick <ggarcia@abonmarche.com>
2024-02-12 17:05:07 -0500

    oauth funtions

--- app.js
+++ app.js
@@ -6,56 +6,79 @@ require([
   "esri/widgets/Editor",
   "esri/identity/OAuthInfo",
   "esri/identity/IdentityManager",
-], function(esriConfig, WebMap, MapView, Legend, Editor, OAuthInfo, esriId) {
+  "esri/portal/Portal",
+], function (WebMap, MapView, Legend, Editor, OAuthInfo, esriId, Portal) {
+  // UI elements
+  const signInButton = document.getElementById("sign-in-button");
+  const navigationUser = document.getElementById("nav-user");
+  signInButton.addEventListener("click", signInOrOut);
+  navigationUser.addEventListener("click", signInOrOut);
 
   //Create a new OAuthInfo object.
   const info = new OAuthInfo({
     appId: "lgmq6kIt5EthgVBO", // Replace with your actual App ID
-    authNamespace: "portal_oauth_inline",
-    popup: true // Use true to have OAuth login in a separate popup
+    popup: true, // Use true to have OAuth login in a separate popup
   });
 
   // Register the OAuthInfo with the IdentityManager.
   esriId.registerOAuthInfos([info]);
 
-  esriId.checkSignInStatus(info.portalUrl + "/sharing").then(
-    function() {
-      console.log("User is signed in");
-      loadMap();
-    }
-  ).catch(
-    function() {
-      console.log("User is not signed in");
-      esriId.getCredential(info.portalUrl + "/sharing").then(
-        function() {
+  // Call the checkSignIn function to see if the user is already signed in.
+  checkSignIn();
+
+  function checkSignIn() {
+    esriId
+      .checkSignInStatus(info.portalUrl + "/sharing")
+      .then(() => {
+        navigationUser.hidden = false;
+        signInButton.hidden = true;
+        const portal = new Portal({
+          authMode: "immediate",
+        });
+
+        portal.load().then(() => {
+          navigationUser.fullName = portal.user.fullName;
+          navigationUser.username = portal.user.username;
+          // Load the map only after successful sign-in and portal loading
           loadMap();
-        }
-      );
-    }
-  );
+        });
+      })
+      .catch(() => {
+        signInButton.hidden = false;
+        navigationUser.hidden = true;
+      });
+  }
+
+  function signInOrOut() {
+    esriId
+      .checkSignInStatus(info.portalUrl + "/sharing")
+      .then(esriId.destroyCredentials)
+      .catch(() => {
+        esriId.getCredential(info.portalUrl + "/sharing").then(checkSignIn);
+      });
+  }
 
   function loadMap() {
     const webmap = new WebMap({
       portalItem: {
-        id: "6446eddb7c284d63a72073626e0a8312" // Replace with your WebMap ID
-      }
+        id: "6446eddb7c284d63a72073626e0a8312",
+      },
     });
 
     const view = new MapView({
       container: "viewDiv",
-      map: webmap
+      map: webmap,
     });
 
-    view.when(function() {
-      // Add widgets here
+    view.when(() => {
       var legend = new Legend({
-        view: view
+        view: view,
       });
 
       view.ui.add(legend, "bottom-left");
 
       var editor = new Editor({
-        view: view
+        view: view,
       });
 
       view.ui.add(editor, "top-right");
