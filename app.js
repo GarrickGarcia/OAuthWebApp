f7c3dc6f4319857c77d1b7fd3e8e1eb062c543b3
Garrick <ggarcia@abonmarche.com>
2024-02-12 09:43:09 -0500

    add legend

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-/*
-require([
-  "esri/config",
-  "esri/WebMap",
-  "esri/views/MapView",
-  "esri/identity/OAuthInfo",
-  "esri/identity/IdentityManager"
-], function(esriConfig, WebMap, MapView, OAuthInfo, esriId) {
-
-  esriConfig.apiKey = "AAPKbf70a5e7fff643aba4086899bea30df3C74r6D4ccA7QYb9fEd340y95U0Px-hJW9N7gkH3sMf5MuKGKAn27wvOnuJc9QV71";
-
-  var info = new OAuthInfo({
-    appId: "lgmq6kIt5EthgVBO",
-    popup: true // Use a popup for the OAuth dialog
-  });
-  esriId.registerOAuthInfos([info]);
-
-  esriId.checkSignInStatus(info.portalUrl + "/sharing").then(
-    function() {
-      loadMap();
-    }
-  ).catch(function() {
-    esriId.getCredential(info.portalUrl + "/sharing");
-  });
-
-  function loadMap() {
-    const webmap = new WebMap({
-      portalItem: {
-        id: "6446eddb7c284d63a72073626e0a8312"
-      }
-    });
-
-    const view = new MapView({
-      container: "viewDiv",
-      map: webmap
-    });
-  }
-});
-*/
-
 require([
   "esri/config",
   "esri/WebMap",
@@ -45,7 +5,7 @@ require([
   "esri/widgets/Legend",
   // "esri/identity/OAuthInfo",
   // "esri/identity/IdentityManager"
-], function(esriConfig, WebMap, MapView) {
+], function(esriConfig, WebMap, MapView, Legend) {
 
   esriConfig.apiKey = "AAPKbf70a5e7fff643aba4086899bea30df3C74r6D4ccA7QYb9fEd340y95U0Px-hJW9N7gkH3sMf5MuKGKAn27wvOnuJc9QV71";
 
@@ -61,7 +21,7 @@ require([
   });
 
   view.when(function() {
-    // Create the Legend without specifying layerInfos
+    // Create the Legend s
     var legend = new Legend({
       view: view
     });
